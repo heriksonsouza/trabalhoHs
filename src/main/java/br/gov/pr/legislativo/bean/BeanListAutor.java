07ccfc74c589b07bc360a7df6cadeaf7da20d700
Casa <Casa@192.168.25.19>
2018-02-12 02:02:13 -0200

    ajuste servidor Tomcat

--- src/main/java/br/gov/pr/legislativo/bean/BeanListAutor.java
+++ src/main/java/br/gov/pr/legislativo/bean/BeanListAutor.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.RequestScoped;
+//import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -12,23 +13,21 @@ import br.gov.pr.legislativo.entidades.Autor;
 import br.gov.pr.legislativo.modelo.servico.ServicoAutor;
 
 @Named
-@ViewScoped
-public class BeanListAutor implements Serializable{
-	/**
-	 * 
-	 */
+@RequestScoped
+// @ViewScoped
+public class BeanListAutor implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	private Autor autor = new Autor();
 	@Inject
 	private ServicoAutor servico;
 	private List<Autor> lista;
-	
+
 	@PostConstruct
-	public void carregar(){
+	public void carregar() {
 		lista = servico.consultar();
 	}
 
-	
 	public Autor getAutor() {
 		return autor;
 	}
@@ -36,7 +35,7 @@ public class BeanListAutor implements Serializable{
 	public void setAutor(Autor autor) {
 		this.autor = autor;
 	}
-	
+
 	public List<Autor> getLista() {
 		return lista;
 	}
@@ -45,12 +44,12 @@ public class BeanListAutor implements Serializable{
 		this.lista = lista;
 	}
 
-	public void excluir(){
+	public void excluir() {
 		servico.excluir(autor.getIdAutor());
 		lista = servico.consultar();
 	}
 
-	public void salvar(){
+	public void salvar() {
 		servico.salvar(autor);
 	}
 }
